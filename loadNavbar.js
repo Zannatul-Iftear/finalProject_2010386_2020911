ecc73dd438b189827d70ebb245d77898f48736c3
Md Zannatul Iftear <125757015+Zannatul-Iftear@users.noreply.github.com>
2023-08-16 06:53:05 +0600

    Add files via upload

---
+++ loadNavbar.js
@@ -0,0 +1,11 @@
+fetch('navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    const navbarPlaceholder = document.getElementById('navbarPlaceholder');
+    navbarPlaceholder.innerHTML = data;
+  });
+
+// Load the navigation script
+const script = document.createElement('script');
+script.src = 'navbar.js';
+document.body.appendChild(script);
